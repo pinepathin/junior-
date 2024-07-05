a10ab18c723aa792a3689cc2a47e79da9dcfe9a2
amani halaby <amani.halaby@phinexes.com>
2024-07-05 11:25:27 +0300

    fix dashboard

--- src/app/user/components/dashboard/dashboard.component.ts
+++ src/app/user/components/dashboard/dashboard.component.ts
@@ -1,40 +1,42 @@
 import {Component, OnInit} from '@angular/core';
+
 import {PostService} from "../../user-services/post-service/post.service";
 import Routes from "../../../utils/routes";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
   // showComments = false;
   liked = false;
+  usertofind: string = '';
+  alluser: any[] = [];
 
   posts: any[] = [];
   pageNum: number = 0;
-  baseBackend :string = Routes.BASE_ENDPOINT;
+  baseBackend: string = Routes.BASE_ENDPOINT;
   total!: number;
-  constructor(private service: PostService) {
-  }
+  constructor(private service: PostService) {}
 
   ngOnInit() {
     this.getAllPosts();
+    this.getUsers();
   }
 
   getAllPosts() {
     this.service.getAllPosts(this.pageNum).subscribe((res: any) => {
       this.posts = res.content;
       this.total = res.totalPages * 5;
-    })
-  }
-
-  deletePost = (postId: number) =>  {
-    this.service.deletePostById(postId).subscribe(res => {
-      this.posts = this.posts.filter((e) => e.id != postId)
     });
   }
+
+  deletePost = (postId: number) => {
+    this.service.deletePostById(postId).subscribe((res) => {
+      this.posts = this.posts.filter((e) => e.id != postId);
+    });
+  };
 
   //The current page index defaults to 0, but can be explicitly set via pageIndex.
   pageIndexChange(event: any) {
@@ -42,4 +44,31 @@ export class DashboardComponent implements OnInit {
     this.getAllPosts();
   }
 
+  getUsers() {
+    
+    fetch('http://localhost:8081/api/auth/all/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + window.localStorage.getItem('c_token'),
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // This returns a promise that resolves with the JSON parsed body text
+      })
+      .then((data) => {
+        /*
+        {
+          PARCIPANTS=[USER1,USER2]
+        }
+  
+  */ this.alluser = data;
+      });
+  
+
+
+  }
 }
